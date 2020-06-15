65e37a4ac7431d72c96937f7429ffed87338979b
Melvin Laguren <melvin@laguren.net>
2020-06-14 21:11:06 -0700

    Removed * from Acceptance Criteria

--- api/lib/process_story.rb
+++ api/lib/process_story.rb
@@ -1,17 +1,20 @@
 class ProcessStory
   include HTTParty
 
-  def initialize(story, credentials)
-    auth_creds = {:username => credentials['email'], :password => credentials['password']}
+  def initialize(key, credentials, options={})
+    if credentials.empty? == false
+      auth_creds = {:username => credentials['email'], :password => credentials['password']}
 
-    response = HTTParty.get("#{ENV['JIRA']}/rest/api/latest/search?jql=key%20%3D%20#{story}",
-                            :basic_auth => auth_creds)
-    json = JSON.parse response.body
-    descr = json['issues'][0]['fields']['description']
-    puts @description
-    @key = story
-    @summary = json['issues'][0]['fields']['summary']
-    @description = json['issues'][0]['fields']['description']
+      response = HTTParty.get("#{ENV['JIRA']}/rest/api/latest/search?jql=key%20%3D%20#{key}",
+                              :basic_auth => auth_creds, :debug_output => $stdout)
+      @json = JSON.parse response.body
+    else
+      @json=JSON.parse options[:issue].to_json
+    end
+    puts @json
+    @key = key
+    @summary = @json['issues'][0]['fields']['summary']
+    @description = @json['issues'][0]['fields']['description']
   end
 
   def to_golden_story
@@ -19,7 +22,7 @@ class ProcessStory
         :key => @key,
         :summary => @summary,
         :narrative => self.narrative,
-        :acceptance_criteria => self.acceptance_criteria
+        :acceptance_criteria => self.acceptance_criteria,
     }.to_json
   end
 
@@ -37,7 +40,8 @@ class ProcessStory
     criteria=[]
     @description.each_line do | line|
       if (line.strip.start_with? "* I should")
-        criteria << line.delete_prefix('*').strip
+        criteria << line.delete_prefix(' *')
+        puts criteria
       end
     end
     return criteria
